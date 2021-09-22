1d02cdea14bed7d7199dbac9ca619ec2fc0fa823
scrive.jack <scrve.jack@awatapu.school.nz>
2021-09-23 09:34:55 +1200

    Updated the clock v3 & v4

--- Clock_v3.py
+++ Clock_v3.py
@@ -8,6 +8,7 @@ def check():
     time.sleep(2)
     if answer is not None:
         return
+    count_down = 10
     timer = 0
     while timer < 29:
         time.sleep(1)
@@ -17,7 +18,9 @@ def check():
             print("20 seconds Left")
         if timer == 20 and answer is None:
             print("10 seconds left")
-
+        if timer > 20 and answer is None:
+            count_down -= 1
+            print("{} seconds left".format(count_down))
 
 Thread(target=check).start()
 
--- Clock_v4.py
+++ Clock_v4.py
@@ -1,27 +1,32 @@
 import time
 from threading import Thread
 
-#sets the answer to none so that the check function
+# sets the answer to none so that the check function
 answer = None
 
+
+# A function that is used to print the time reminders even if an input is asked
 def check():
-    if answer is not None:
-        return
     timer = 0
-    while timer < 29:
+    while timer < 39 and answer is None:
         time.sleep(1)
         timer += 1
-
-        if timer == 10 and answer is None:
+        if answer is not None:
+            return
+        if timer == 10:
             print("\n20 seconds Left")
-        if timer == 20 and answer is None:
+        if timer == 20:
             print("\n10 seconds left")
+        if timer == 29:
+            print("\nTimes up")
+        if timer == 38:
+            print("\n"*20)
 
-
+# calls the check function to print time reminders
 Thread(target=check).start()
 
+# asks the user a question then waits for a response
 answer = input("Input something: ")
 
 if answer == "nine":
     print("correct")
-
