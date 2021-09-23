adf831fb646066580d0bc68951ccf528d5d1ef1d
scrive.jack <scrve.jack@awatapu.school.nz>
2021-09-24 10:47:40 +1200

    Re wrote the Answer Checker and ask'er_function.py because it was garbage

--- Answer Checker and ask'er_function.py
+++ Answer Checker and ask'er_function.py
@@ -1,23 +1,21 @@
-
-def answer_chcekcer(question_num,answer):
+def answer_checkcer(input_answer, question_num,):
     answers = ["answer 1", "answer 2", "answer 3"]
-    score = 0
-    if answer == answers[question_num]:
-        return "Correct"
+    if input_answer == answers[question_num]:
+        return True
     else:
-        return ("Almost the answer was {}".format(answers[question_num]))
+        return False
 
 
+score = 0
+question_num = 0
+answers = ["answer 1", "answer 2", "answer 3"]
 questions = ["question 1", "question 2", "question 3", ]
-question_num_1 = 0
 for i in questions:
-    answer = input("The question is {}".format(questions[question_num_1]))
-    correct = answer_chcekcer(question_num_1,answer)
-    question_num_1 += 1
+    answer = input(questions[question_num])
+    correct = answer_checkcer(answer, question_num)
+    if correct:
+        print("correct")
+    else:
+        print("incorrect")
+    question_num += 1
 
-
-
-if score == 1:
-    print("you have {} point".format(score))
-else:
-   print("you have {} points".format(score))
