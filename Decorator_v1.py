0e9c7f615e42b10e37f6649611c289c0999bf8cf
scrive.jack <scrve.jack@awatapu.school.nz>
2021-09-29 14:36:31 +1300

    Updated the Decorator_v1.py and Decorator_v2.py files

--- Decorator_v1.py
+++ Decorator_v1.py
@@ -3,4 +3,7 @@ def decorator(input_text):
     print("*"*4+input_text+"*"*4)
     print("*"*4*2+"*"*len(input_text))
 
+
 decorator("Test_text")
+
+
--- Decorator_v2.py
+++ Decorator_v2.py
@@ -1,7 +1,14 @@
+# A function that decorates text that is imputed when it is called
 def decorator(input_text, num_of_deco, deco_type):
+    # A print statement that prints the number of the chosen symbol twice plus 1 for every character in the text imputed
     print(deco_type * num_of_deco * 2 + deco_type * len(input_text))
+    # A print statement that prints the number of the chosen symbols on either side of the input text and the input text
     print(deco_type * num_of_deco + input_text + deco_type * num_of_deco)
+    # A print statement that prints the number of the chosen symbol twice plus 1 for every character in the text imputed
     print(deco_type * num_of_deco * 2 + deco_type * len(input_text))
 
 
+# Calls the decorator function and decorates the text in this case Test_text with 2 & symbols on either side, above and bellow
 decorator("Test_text", 2, "&")
+
+
